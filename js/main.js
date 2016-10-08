b998b9d08a7e8df18d802fa34d0552ce291f1ce6
Muhammad-MuZzammil <Muhammadmuzzammil10@hotmail.com>
2016-10-09 03:08:52 +0500

    add animation

--- js/main.js
+++ js/main.js
@@ -7,4 +7,57 @@ $(document).ready(function(){
             $('.navbar').removeClass('navbar-fixed-top');
         }
     });
-})
+});
+
+
+$(".navbar-dark ul li a[href^='#']").on('click',function(e){
+    
+    e.preventDefault();
+    
+    var hash=this.hash;
+    
+    $('html, body').animate({
+        scrollTop:$(hash).offset().top
+    }, 2000, function(){
+        
+        window.location.hash = hash;
+    });
+    
+});
+
+$(" #section-parallax .col-sm-8 a[href^='#']").on('click',function(e){
+    
+    e.preventDefault();
+    
+    var hash=this.hash;
+    
+    $('html, body').animate({
+        scrollTop:$(hash).offset().top
+    }, 2000, function(){
+        
+        window.location.hash = hash;
+    });
+    
+});
+
+
+$(".footer .scroll-to-top-button a[href^='#']").on('click',function(e){
+    
+    e.preventDefault();
+    
+    var hash=this.hash;
+    
+    $('html, body').animate({
+        scrollTop:$(hash).offset().top
+    }, 2000, function(){
+        
+        window.location.hash = hash;
+    });
+    
+});
+
+$('.js-wp-1').waypoint(function(direction){
+    $('.js-wp-1').addClass('animated fadeInDown');
+}, {
+    offset: '50%'
+});
